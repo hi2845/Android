21e8fc492f90ad3330b74427644d8e29f30544c5
kyungminJ <31791800+kyungminJ@users.noreply.github.com>
2017-10-16 17:11:16 +0900

    위로가기 버튼 설정

--- app/src/main/java/com/example/hi284/androidproject/ManuActivity.java
+++ app/src/main/java/com/example/hi284/androidproject/ManuActivity.java
@@ -1,6 +1,9 @@
 package com.example.hi284.androidproject;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.Drawable;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -22,6 +25,16 @@ public class ManuActivity extends AppCompatActivity {
         iv.setImageResource(intent.getIntExtra("img", 0));
         tvTitle.setText(intent.getStringExtra("name"));
         tvArtist.setText(intent.getStringExtra("price"));
+
+        ActionBar actionBar = getSupportActionBar();
+        if(actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            Drawable drawable = getDrawable(R.drawable.ic_keyboard_arrow_left_black_24dp);
+            if(drawable != null) {
+                drawable.setTint(Color.WHITE);
+                actionBar.setHomeAsUpIndicator(drawable);
+            }
+        }
 
     }
 }
