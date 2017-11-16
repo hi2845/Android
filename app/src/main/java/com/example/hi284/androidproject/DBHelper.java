9267c599ac51dea7b775cf01b7b728955908ef9f
kyungminJ <31791800+kyungminJ@users.noreply.github.com>
2017-11-16 14:52:51 +0900

    식당에 관한 Table 관리
    1. 식당추가하면 Rests 테이블에 식당이름, 식당번호, 식당사진에 대한 URI의 문자열을 저장.
    2. Rests 테이블에 있는 모든 정보 불러오기.
    3. 조건에 맞는 Rests 테이블의 정보 불러오기.

--- app/src/main/java/com/example/hi284/androidproject/DBHelper.java
+++ app/src/main/java/com/example/hi284/androidproject/DBHelper.java
@@ -1,7 +1,10 @@
 package com.example.hi284.androidproject;
 
+import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 
 /**
@@ -10,6 +13,8 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
     final static String TAG = "DataBase";
+    final int SUCCESS = 1;
+    final int FAILS = -1;
 
     public DBHelper(Context context) {
         super(context, ResContract.DB_NAME, null, ResContract.DATABASE_VERSION);
@@ -23,5 +28,62 @@ public class DBHelper extends SQLiteOpenHelper {
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
         sqLiteDatabase.execSQL(ResContract.Rests.DELETE_TABLE);
+        onCreate(sqLiteDatabase);
     }
+
+    // Rests 테이블에 있는 모든 정보 가져오기
+    public Cursor getAllRest() {
+        SQLiteDatabase db = getReadableDatabase();
+        return db.query(ResContract.Rests.REST_TABLE_NAME,null,null,null,null,null,null);
+    }
+
+    // 조건에 합하는 식당이 있는지 검사
+    public Cursor getRest(String resName, String resNum) {
+        SQLiteDatabase db = getReadableDatabase();
+        String sql = String.format(
+                "SELECT * FROM %s WHERE %s = '%s' AND %s = '%s'",
+                ResContract.Rests.REST_TABLE_NAME,
+                ResContract.Rests.REST_NAME,
+                resName,
+                ResContract.Rests.REST_NUM,
+                resNum
+                );
+        try {
+            Cursor c = getReadableDatabase().rawQuery(sql, null);
+            return c;
+        } catch (SQLiteException e) {
+            return null;
+        }
+    }
+
+    // Rests 테이블에 식당 추가 (식당이름, 식당번호, 식당사진에 대한 URI를 문자열로 받음)
+    public int insertRest(String resName, String resNum, String resPic) {
+        SQLiteDatabase db = getWritableDatabase();
+        ContentValues values = new ContentValues();
+
+        if(getRest(resName, resNum) != null) {
+            values.put(ResContract.Rests.REST_NAME, resName);
+            values.put(ResContract.Rests.REST_NUM, resNum);
+            values.put(ResContract.Rests.REST_PIC, resPic);
+
+            db.insert(ResContract.Rests.REST_TABLE_NAME, null, values);
+            if(createRestTable(resName) == SUCCESS) {
+                return SUCCESS;
+            } else
+                return FAILS;
+        } else {
+            return FAILS;
+        }
+    }
+
+    /*
+    // Rests 테이블에 식당 삭제 (식당이름, 식당번호를 문자열로 받음)
+    public long deleteRest(String resName, String resNum) {
+        SQLiteDatabase db = getWritableDatabase();
+
+        String whereClause = ResContract.Rests.REST_NAME +" = ? AND " + ResContract.Rests.REST_NUM+" = ?";
+        String[] whereArgs ={resName, resNum};
+        return db.delete(ResContract.Rests.REST_TABLE_NAME, whereClause, whereArgs);
+    }
+    */
 }
