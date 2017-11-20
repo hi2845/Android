4dca2050d50ae4e80d9778169025662d99282063
hi2845 <hi2845@gmail.com>
2017-11-21 08:10:52 +0900

    Revert "프래그먼트 수정"
    This reverts commit 24689f125974e570cb270b454bc16e7d01315cb5.

--- app/src/main/java/com/example/hi284/androidproject/Restaurant.java
+++ app/src/main/java/com/example/hi284/androidproject/Restaurant.java
@@ -1,7 +1,6 @@
 package com.example.hi284.androidproject;
 
 import android.content.Intent;
-import android.content.res.Configuration;
 import android.database.Cursor;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -12,96 +11,107 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 
-public class Restaurant extends AppCompatActivity implements AdapterView.OnItemClickListener, RestFragment.OnTitleSelectedListener {
+public class Restaurant extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private DBHelper mDbHelper;
-
-    int id;
-
-
-    String rName;
-    String rPhone;
-    String rAddr;
-    String pic;
-
-    String svname;
-    String svphone;
-    String svaddr;
-
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant);
 
-        /*mDbHelper = new DBHelper(this);
 
-        //정보를 받아온후 저장
-        id=getIntent().getIntExtra("KEY",0);
-
-        if(getIntent().getStringExtra("name")!=null){
-            rName=getIntent().getStringExtra("name");}
-        if(getIntent().getStringExtra("phone")!=null){
-            rPhone=getIntent().getStringExtra("phone");}
-        if(getIntent().getStringExtra("addr")!=null){
-            rAddr=getIntent().getStringExtra("addr");}
-
-        //insertRest();
+        Intent intent= getIntent();
+        int rewID=getIntent().getIntExtra("KEY", 0);
 
 
+        ImageButton ib = (ImageButton) findViewById(R.id.call_btn);
+        ib.setImageResource(R.drawable.call);
+        ib.setScaleType(ImageButton.ScaleType.FIT_XY);
 
-        //레스토랑 프래그먼트 실행
-        RestFragment rest= new RestFragment();
-        if(rName!=null){
-        rest.setrName(rName);}
-        if(rPhone!=null){
-        rest.setrPhone(rPhone);}
-        if(rAddr!=null){
-        rest.setrAddr(rAddr);}
-        getSupportFragmentManager().beginTransaction().replace(R.id.title, rest).commit();*/
+        // string.xml 리소스파일에서 식당이미지 불러오기
+        ImageView im = (ImageView)findViewById(R.id.res_img);
+        im.setImageResource(R.drawable.res1);
+        im.setScaleType(ImageView.ScaleType.FIT_XY);
 
+
+        // 리소스파일에서 식당이름 불러오기
+        TextView tvName = (TextView)findViewById(R.id.res_tv);
+        tvName.setText(intent.getStringExtra("name"));
+
+
+        // string.xml 리소스파일에서 식당연락처 불러오기
+        TextView tvNum = (TextView)findViewById(R.id.res_num);
+        tvNum.setText(intent.getStringExtra("phone"));
+
+
+        // string.xml 리소스파일에서 식당주소 불러오기
+        TextView tvAddr = (TextView)findViewById(R.id.res_addr);
+        tvAddr.setText(intent.getStringExtra("addr"));
+
+
+
+        // 어댑터 생성
+        ListAdapter adapters = createAdapter();
+
+        final ArrayList<MyItem> data = new ArrayList<>();
+        data.add(new MyItem(R.drawable.mu1,"물냉면","9000원"));
+        data.add(new MyItem(R.drawable.mu2,"비빔냉면","9000원"));
+        data.add(new MyItem(R.drawable.mu3,"왕갈비탕","12000원"));
+        data.add(new MyItem(R.drawable.mu4,"갈비","진 6만원 선 5만원 미 4만원"));
+
+        // 어댑터 연결
+        ListView listv = (ListView)findViewById(R.id.res_menu);
+        listv.setAdapter(adapters);
+        listv.setDivider(new ColorDrawable(Color.GRAY));
+        listv.setDividerHeight(5);
+        listv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+
+                Intent intent = new Intent(
+                        getApplicationContext(), // 현재화면의 제어권자
+                        ManuActivity.class); // 다음넘어갈 화면
+
+                // intent 객체에 데이터를 실어서 보내기
+                // 리스트뷰 클릭시 인텐트 (Intent) 생성하고 position 값을 이용하여 인텐트로 넘길값들을 넘긴다
+                intent.putExtra("img", data.get(position).mIcon);
+                intent.putExtra("name", data.get(position).nName);
+                intent.putExtra("price", data.get(position).nprice);
+
+                startActivity(intent);
+            }
+        });
+    } // end of onCreate
+
+
+
+
+
+    //*********************** 여기를 고치시면 될 듯 ***********************
+    // 따로 어댑터를 만드셔도 됩니다. 이건 모양을 위해 그냥 넣어놓은거라...
+    private ListAdapter createAdapter() {
+        ArrayList<MyItem> data = new ArrayList<>();
+        data.add(new MyItem(R.drawable.mu1,"물냉면","9000원"));
+        data.add(new MyItem(R.drawable.mu2,"비빔냉면","9000원"));
+        data.add(new MyItem(R.drawable.mu3,"왕갈비탕","12000원"));
+        data.add(new MyItem(R.drawable.mu4,"갈비","진 6만원 선 5만원 미 4만원"));
+
+        MyAdapter adapt
+                = new MyAdapter(this, R.layout.item,data);
+
+
+
+        return adapt;
     }
-
-
-     // end of onCreate
-
-    @Override
-    public void onTitleSelected(int pic,String name, String price,String rName, String rPhone ,String rAddr) {
-        if (getResources().getConfiguration().orientation
-                == Configuration.ORIENTATION_LANDSCAPE) {
-
-            //landscape 상태이면 fragment_restaurant(land)의 container에 메뉴프래그먼트가 들어간다
-
-            ManuFragment manus = new ManuFragment();
-            manus.setpic(pic);
-            manus.setname(name);
-            manus.setprice(price);
-            getSupportFragmentManager().beginTransaction().replace(R.id.container, manus).commit();
-        } else {
-            Intent intent = new Intent(
-                    getApplicationContext(), // 현재화면의 제어권자
-                    ManuActivity.class); // 다음넘어갈 화면
-
-            // intent 객체에 데이터를 실어서 보내기
-            // 리스트뷰 클릭시 인텐트 (Intent) 생성하고 position 값을 이용하여 인텐트로 넘길값들을 넘긴다
-            intent.putExtra("img",pic);
-            intent.putExtra("name", name);
-            intent.putExtra("price", price);
-            startActivity(intent);
-        }
-    }
-
-
 
     @Override
     public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -115,31 +125,4 @@ public class Restaurant extends AppCompatActivity implements AdapterView.OnItemC
         Intent intent1 = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+call));
         startActivity(intent1);
     }
-
-    private void insertRest() {
-        String name=rName;
-        String phone=rPhone;
-        String addr=rAddr;
-        String img=pic;
-
-
-        long nOfRows = mDbHelper.insertRest(name,phone,addr,pic);
-        if (nOfRows >0)
-            Toast.makeText(this,nOfRows+" Record Inserted", Toast.LENGTH_SHORT).show();
-        else
-            Toast.makeText(this,"No Record Inserted", Toast.LENGTH_SHORT).show();
-    }
-
-    @Override
-
-
-    protected void onSaveInstanceState(Bundle outState) {
-
-        // TODO Auto-generated method stub
-        super.onSaveInstanceState(outState);
-
-
-    }
-
-
 }
