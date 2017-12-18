eab672cdae5038db9c2220301fb68c5a0d823166
kyungminJ <31791800+kyungminJ@users.noreply.github.com>
2017-12-18 22:51:26 +0900

    데이터베이스 수정
    식당 추가할 때 위도,경도값도 추가
    같은 위도, 경도를 가진 식당찾는 메소드 추가

--- app/src/main/java/com/example/hi284/androidproject/DBHelper.java
+++ app/src/main/java/com/example/hi284/androidproject/DBHelper.java
@@ -42,6 +42,22 @@ public class DBHelper extends SQLiteOpenHelper {
         return db.query(ResContract.Rests.REST_TABLE_NAME,null,null,null,null,null,null);
     }
 
+    // Marker 식당 검사
+    public Cursor checkRest(String resLat, String resLng) {
+        SQLiteDatabase db = getReadableDatabase();
+        String sql = String.format(
+                "SELECT * FROM %s WHERE %s = '%s' A",
+                ResContract.Rests.REST_TABLE_NAME,
+                ResContract.Rests.REST_LAT,
+                resLat,
+                ResContract.Rests.REST_LNG,
+                resLng
+        );
+
+        Cursor c = db.rawQuery(sql, null);
+        return c;
+    }
+
     // 조건에 합하는 식당이 있는지 검사
     public Cursor getRest(String resName, String resNum) {
         SQLiteDatabase db = getReadableDatabase();
@@ -50,31 +66,28 @@ public class DBHelper extends SQLiteOpenHelper {
                 ResContract.Rests.REST_TABLE_NAME,
                 ResContract.Rests.REST_NUM,
                 resNum
-                );
+        );
 
         Cursor c = db.rawQuery(sql, null);
         return c;
     }
 
     // Rests 테이블에 식당 추가 (식당이름, 식당번호, 식당주소, 식당사진에 대한 URI를 문자열로 받음)
-    public int insertRest(String resName, String resNum, String resAddr, String resPic) {
+    public int insertRest(String resName, String resNum, String resAddr, String resLat, String resLng, String resPic) {
         SQLiteDatabase db = getWritableDatabase();
         ContentValues values = new ContentValues();
 
-        if(getRest(resName, resNum).getCount() == 0) {
-            values.put(ResContract.Rests.REST_NAME, resName);
-            values.put(ResContract.Rests.REST_NUM, resNum);
-            values.put(ResContract.Rests.REST_ADDR, resAddr);
-            values.put(ResContract.Rests.REST_PIC, resPic);
+        values.put(ResContract.Rests.REST_NAME, resName);
+        values.put(ResContract.Rests.REST_NUM, resNum);
+        values.put(ResContract.Rests.REST_ADDR, resAddr);
+        values.put(ResContract.Rests.REST_LAT, resLat);
+        values.put(ResContract.Rests.REST_LNG, resLng);
+        values.put(ResContract.Rests.REST_PIC, resPic);
 
-            db.insert(ResContract.Rests.REST_TABLE_NAME, null, values);
-            if(createRestTable(resName) == SUCCESS) {
-                return SUCCESS;
-            } else
-                return FAILS;
-        } else {
-            return FAILS;
-        }
+        db.insert(ResContract.Rests.REST_TABLE_NAME, null, values);
+        createRestTable(resName);
+
+        return SUCCESS;
     }
 
     // 추가한 식당에 대한 메뉴테이블 만들기(식당이름을 문자열로 받음)
@@ -89,9 +102,9 @@ public class DBHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = getWritableDatabase();
 
         String sql = "CREATE TABLE " + TABLE_NAME + " (" +
-                        MENU + TEXT_TYPE + COMMA_SEP +
-                        MENU_PRICE + TEXT_TYPE + COMMA_SEP +
-                        MENU_PIC + TEXT_TYPE + " )";
+                MENU + TEXT_TYPE + COMMA_SEP +
+                MENU_PRICE + TEXT_TYPE + COMMA_SEP +
+                MENU_PIC + TEXT_TYPE + " )";
         try {
             db.execSQL(sql);
             return SUCCESS;
@@ -136,7 +149,6 @@ public class DBHelper extends SQLiteOpenHelper {
         }
     }
 
-
     // Rests 테이블에 식당 삭제 (식당이름, 식당번호를 문자열로 받음)
     public long deleteRest(String resName, String resNum) {
         SQLiteDatabase db = getWritableDatabase();
@@ -156,42 +168,4 @@ public class DBHelper extends SQLiteOpenHelper {
             return -1;
         }
     }
-
-    public long updateUserByMethod(String _id, String name, String phone) {
-        SQLiteDatabase db = getWritableDatabase();
-
-        ContentValues values = new ContentValues();
-        values.put(ResContract.Rests.REST_NAME, name);
-        values.put(ResContract.Rests.REST_NUM,phone);
-
-        String whereClause = ResContract.Rests._ID +" = ?";
-        String[] whereArgs ={_id};
-
-        return db.update(ResContract.Rests.REST_TABLE_NAME, values, whereClause, whereArgs);
-    }
-    public Cursor fetchBook(long rowID) throws SQLException {
-        Cursor Cursor =
-                db.query(true, ResContract.Rests.CREATE_TABLE, new String[]{
-                        ResContract.Rests.REST_NAME,ResContract.Rests.REST_NUM,ResContract.Rests.REST_ADDR},
-                        ResContract.Rests._ID + "=" + rowID, null, null, null, null, null);
-
-        if(Cursor != null)
-            Cursor.moveToFirst();
-
-        return Cursor;
-
-    }
-    public RestClass getPersonById(int _id)
-    { StringBuffer sb = new StringBuffer();
-    sb.append(" SELECT NAME, AGE, PHONE, ADDRESS FROM TEST_TABLE WHERE _ID = ? ");
-    SQLiteDatabase db = getReadableDatabase();
-    Cursor cursor = db.rawQuery(sb.toString(), new String[]{_id + ""});
-    RestClass restClass= new RestClass();
-    if(cursor.moveToNext())
-    {
-        restClass.setName(cursor.getString(0));
-        restClass.setPhone(cursor.getString(1));
-        restClass.setAddr(cursor.getString(2));
-    }
-    return restClass; }
 }
